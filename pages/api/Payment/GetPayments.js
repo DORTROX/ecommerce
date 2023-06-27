cc1af530bd8437a4cf07af0bdeffa356da115b42
DORTROX プロ <levisaustin011@gmail.com>
2023-06-27 11:58:38 +0530

    Sort

--- pages/api/Payment/GetPayments.js
+++ pages/api/Payment/GetPayments.js
@@ -5,7 +5,7 @@ export default async function GetPayemnts(req, res) {
 
   const { enums } = req.body;
   try {
-    const payments = await Orders.find().skip(enums * 5).limit(5);
+    const payments = await Orders.find().skip(enums * 5).limit(5).sort({created_at: -1});
     res.status(200).json(payments);
   } catch (error) {
     res.status(400).json(error);
